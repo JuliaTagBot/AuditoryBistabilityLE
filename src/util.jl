d99acfe4e4a4311f9afaeb2224f25d55d869e239
David Little <david.frank.little@gmail.com>
2019-05-31 16:07:48 -0400

    cleaned up `as_namedtuple`

--- src/util.jl
+++ src/util.jl
@@ -2,9 +2,9 @@ read_settings(x) = as_namedtuple(x)
 read_settings(x::String) = as_namedtuple(TOML.parsefile(x))
 as_namedtuple(xs) = xs
 function as_namedtuple(xs::Dict{<:AbstractString,<:Any})
-  kt = ((Symbol(x) for x in keys(xs))...,)
-  vt = (values(xs)...,)
-  NamedTuple{(kt...,)}(as_namedtuple.(vt))
+  kt = Tuple((Symbol(x) for x in keys(xs)))
+  vt = Tuple(values(xs))
+  NamedTuple{kt}(as_namedtuple.(vt))
 end
 
 read_params(x) = x
